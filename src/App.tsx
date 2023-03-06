39f3bc45f79ba038e8be68307a31b10f7902b9a8
Wei Shen <weishen1990@gmail.com>
2023-03-07 02:37:32 +0800

    Setup Webview

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React from 'react';
+import React, { useCallback } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -24,94 +25,53 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import WebView, { WebViewMessageEvent } from 'react-native-webview';
+import LoadingIndicator from './LoadingIndicator';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const LEAFLET_HTML_SOURCE = Platform.select({
+  ios: require('../android/app/src/main/assets/index.html'),
+  android: {uri: 'file:///android_asset/index.html'},
+});
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const handleMessage = useCallback(
+    (event: WebViewMessageEvent) => {
+      const data = event?.nativeEvent?.data;
+      if (!data) {
+        return;
+      }
+      const message = JSON.parse(data);
+      console.log(`received: ${JSON.stringify(message)}`);
+    },
+    []
+  );
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={{flex:1}}>
+      <WebView
+        containerStyle={styles.container}
+        javaScriptEnabled={true}
+        onMessage={handleMessage}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+
+        originWhitelist={['*']}
+        renderLoading={() => <LoadingIndicator/>}
+        source={LEAFLET_HTML_SOURCE}
+        allowFileAccess={true}
+        allowUniversalAccessFromFileURLs={true}
+        allowFileAccessFromFileURLs={true}
+        androidHardwareAccelerationDisabled={false}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+
+  container: {
+    flex: 1,
+    // ...StyleSheet.absoluteFillObject,
   },
 });
 
